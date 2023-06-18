c14ac0fc85f6977ef378747e28429640d6bb9d0e
Artem Baguinski <abaguinski@gmail.com>
2023-06-18 06:46:11 +0000

    Skip more irrelevant folders during installation

--- scratch/dev.sh
+++ scratch/dev.sh
@@ -1,2 +1,3 @@
+npm i
 # send it to the test vault
-rsync ./ /dist -rvu --exclude=dist --exclude=node_modules --delete --delete-excluded
+rsync ./ /dist -rvu --exclude=dist --exclude=node_modules --exclude .git --exclude .devcontainer --delete --delete-excluded
